de3c14f47a20f38aeacaa94897aceb753dd3c352
Jason Koh <bk7749@gmail.com>
2020-03-16 08:26:39 +0000

    fix bugs
    update
    
    fix bug
    
    modularize auth mechanism

--- brick_server/auth/models.py
+++ brick_server/auth/models.py
@@ -1,6 +1,10 @@
 from typing import List, Dict, Any
+from pydantic import BaseModel, Field
+
+class TokenResponse(BaseModel):
+    token: str = Field(..., description='JWT token')
+    name: str = Field(..., description='Associated name with the token')
+    exp: int = Field(..., description='The token\'s expiration time in unix timestamp')
 
 
-
-TokenResponse = str
 TokensResponse = List[TokenResponse]
