5c31dc1b0043b3d81b7dd55f18c3c1d4c0b51d1c
Jason Koh <bk7749@gmail.com>
2020-03-19 21:13:39 +0000

    more generalize auth logic

--- brick_server/auth/authorization.py
+++ brick_server/auth/authorization.py
@@ -78,7 +78,7 @@ def create_jwt_token(app_name: str,
     payload = {
         'user_id': 'admin',
         'exp': time.time() + token_lifetime, # TODO: Think about the timezone
-        'app': app_name,
+        'app_id': app_name,
     }
     jwt_token = jwt.encode(payload, _jwt_priv_key, algorithm='RS256')
     return jwt_token
@@ -95,7 +95,13 @@ def check_admin(*args, **kwargs):
     else:
         return True
 
-def validate_token(*args, **kwargs):
+
+# A list of auth_logics
+
+def auth_logic_template(action_type, target_ids, *args, **kwargs):
+    raise Exception('Not Implemented and this is not meant to be used but just for reference.')
+
+def validate_token(action_type, target_ids, *args, **kwargs):
     try:
         payload = parse_jwt_token(kwargs['token'].credentials)
     except jwt.exceptions.InvalidSignatureError as e:
@@ -115,13 +121,18 @@ def authorized_admin(f):
         return await f(*args, **kwargs)
     return decorated
 
-def authorized_arg(permission_type):
+def default_get_target_ids(*args, **kwargs):
+   return [kwargs['entity_id']]
+
+def authorized_arg(permission_type, get_target_ids=default_get_target_ids):
     def auth_wrapper(f):
         @wraps(f)
         async def decorated(*args, **kwargs):
             # Intentionally empty not to check anything as a dummy authorization
             self = kwargs['self']
-            if not self.auth_logic(*args, **kwargs):
+            #jwt_token = parse_jwt_token(kwargs['token'].credentials)
+            target_ids = get_target_ids(*args ,**kwargs)
+            if not self.auth_logic(permission_type, target_ids, *args, **kwargs):
                 raise HTTPException(status_code=401,
                                     detail='{user_id} does not have the right permission.',
                                     )
@@ -134,7 +145,8 @@ def authorized(f):
     async def decorated(*args, **kwargs):
         # Intentionally empty not to check anything as a dummy authorization
         self = kwargs['self']
-        if not self.auth_logic(*args, **kwargs):
+        #jwt_token = parse_jwt_token(kwargs['token'].credentials)
+        if not self.auth_logic(None, [], *args, **kwargs):
             raise HTTPException(status_code=401,
                                 detail='{user_id} does not have the right permission.',
                                 )
