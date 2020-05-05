9fd78522395c1f40790c8843af50c3cddd383f84
Jason Koh <bk7749@gmail.com>
2020-05-05 18:03:38 +0000

    more printing

--- tools/upgrade_brick_users.py
+++ tools/upgrade_brick_users.py
@@ -7,7 +7,9 @@ from brick_server.models import User as BrickUser
 from playground.models import User
 
 for user in BrickUser.objects():
+    print('previously: {0}'.format(user.to_mongo()))
     new_user = User(**user.to_mongo())
     user.delete()
     new_user.save()
+    print('now: {0}'.format(new_user.to_mongo()))
     #activated_apps = ListField(ReferenceField(App), default=[])
