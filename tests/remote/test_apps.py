b464eba99b68e84ddc67a2e57ac098ce5edd32e6
Jason Koh <bk7749@gmail.com>
2020-05-25 01:11:11 +0000

    update

--- tests/remote/test_apps.py
+++ tests/remote/test_apps.py
@@ -7,10 +7,19 @@ from .common import APP_BASE, authorize_headers, BRICK, app_manifest, requests_p
 from .data import znt_id
 
 
-def test_stage_app():
+def test_stage_bacnet_driver():
     headers = authorize_headers()
     body = {
         'app_name': 'bacnet_driver',
+        'app_lifetime': 15552000, # 6 months
+    }
+    resp = requests_post(APP_BASE + '/', json=body, headers=headers)
+    assert resp.status_code in [200, 409]
+
+def test_stage_app1():
+    headers = authorize_headers()
+    body = {
+        'app_name': 'app1',
         'app_lifetime': 15552000, # 6 months
     }
     resp = requests_post(APP_BASE + '/', json=body, headers=headers)
