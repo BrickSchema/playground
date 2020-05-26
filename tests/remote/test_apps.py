43b4a35a0a33cb4897370edf013198a10ace04f2
Jason Koh <bk7749@gmail.com>
2020-05-26 22:52:12 +0000

    tryout

--- tests/remote/test_apps.py
+++ tests/remote/test_apps.py
@@ -3,7 +3,7 @@ import yaml
 import pdb
 from pdb import set_trace as bp
 
-from .common import APP_BASE, authorize_headers, BRICK, app_manifest, requests_post
+from .common import APP_BASE, authorize_headers, BRICK, app_manifest, requests_post, get_session
 from .data import znt_id
 
 
@@ -13,7 +13,9 @@ def test_stage_bacnet_driver():
         'app_name': 'bacnet_driver',
         'app_lifetime': 15552000, # 6 months
     }
-    resp = requests_post(APP_BASE + '/', json=body, headers=headers)
+    url = APP_BASE + '/'#, json=body, headers=headers)
+    with get_session() as session:
+        resp = session.post(authorized_url, json=body, headers=headers)
     assert resp.status_code in [200, 409]
 
 def test_stage_app1():
