ce6741d3a0bacdfb058a4666215af07624e7d8ce
tc-imba <liuyh970615@gmail.com>
2023-08-26 01:36:34 +0800

    feat: add static data
    Co-authored-by: tc-imba <liuyh970615@gmail.com>
    Co-authored-by: Reapor-Yurnero <reapor.yurnero@gmail.com>

---
+++ static/genie/precache-manifest.235a7221216af66b0ca9824e6c97f004.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "144dff078941b37d32c7",
+    "url": "/brickapi/v1/apps/genie/static/static/css/main.978e271f.chunk.css"
+  },
+  {
+    "revision": "144dff078941b37d32c7",
+    "url": "/brickapi/v1/apps/genie/static/static/js/main.507aa101.chunk.js"
+  },
+  {
+    "revision": "8591f7a9706bedd54d0f",
+    "url": "/brickapi/v1/apps/genie/static/static/js/runtime~main.ec56d2a2.js"
+  },
+  {
+    "revision": "cc172fcaeae1d5556565",
+    "url": "/brickapi/v1/apps/genie/static/static/css/2.ccfb1667.chunk.css"
+  },
+  {
+    "revision": "cc172fcaeae1d5556565",
+    "url": "/brickapi/v1/apps/genie/static/static/js/2.cebb0441.chunk.js"
+  },
+  {
+    "revision": "55938600731a104978b3bcaf02ae2e90",
+    "url": "/brickapi/v1/apps/genie/static/static/media/genie_logo.55938600.png"
+  },
+  {
+    "revision": "e4dee3fdcd0eda1c79eb3db4eac1d453",
+    "url": "/brickapi/v1/apps/genie/static/static/media/synergylog_noletter_trans.e4dee3fd.png"
+  },
+  {
+    "revision": "f54aea5b6209b32d25e999e0d6a9e79b",
+    "url": "/brickapi/v1/apps/genie/static/static/media/mesllogo_noletter_trans.f54aea5b.png"
+  },
+  {
+    "revision": "4b00722633d9e6a4c59ab218c73eabc7",
+    "url": "/brickapi/v1/apps/genie/static/static/media/CSELogo_globe_color_trans.4b007226.png"
+  },
+  {
+    "revision": "057ec865a919f40b2c34726164ad0f6c",
+    "url": "/brickapi/v1/apps/genie/static/static/media/ucsd-logo.057ec865.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/brickapi/v1/apps/genie/static/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/brickapi/v1/apps/genie/static/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/brickapi/v1/apps/genie/static/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/brickapi/v1/apps/genie/static/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/brickapi/v1/apps/genie/static/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/brickapi/v1/apps/genie/static/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/brickapi/v1/apps/genie/static/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/brickapi/v1/apps/genie/static/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/brickapi/v1/apps/genie/static/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/brickapi/v1/apps/genie/static/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/brickapi/v1/apps/genie/static/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/brickapi/v1/apps/genie/static/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/brickapi/v1/apps/genie/static/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/brickapi/v1/apps/genie/static/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/brickapi/v1/apps/genie/static/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/brickapi/v1/apps/genie/static/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "5df8f94783bb145fb471e435b5fbff60",
+    "url": "/brickapi/v1/apps/genie/static/static/media/14.5df8f947.jpg"
+  },
+  {
+    "revision": "289c14e1c99a0067253c53079c25ceb3",
+    "url": "/brickapi/v1/apps/genie/static/static/media/wallpaper.289c14e1.jpg"
+  },
+  {
+    "revision": "8e8e625f12e45ecc674f9579c4547351",
+    "url": "/brickapi/v1/apps/genie/static/static/media/DS-DIGII.8e8e625f.TTF"
+  },
+  {
+    "revision": "92c65c0e189e14df8a15ad9f1db98ef2",
+    "url": "/brickapi/v1/apps/genie/static/index.html"
+  }
+];
